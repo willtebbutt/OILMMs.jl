af9926a0b583b86f2d9fedc4a3bd7c7683282694
WT <wt0881@my.bristol.ac.uk>
2021-04-16 14:08:50 +0100

    Re-enable all standard tests

--- test/oilmm.jl
+++ test/oilmm.jl
@@ -45,7 +45,6 @@ end
             σ²=σ²,
         )
     end
-
     @testset "P independent processes" begin
         rng = MersenneTwister(123456)
         P = 3
@@ -91,32 +90,53 @@ end
             σ²=σ²,
         )
     end
+    @testset "Full Rank, Dense H" begin
+        rng = MersenneTwister(123456)
+        P = 3
+        N = 15
 
-    # @testset "Full Rank, Dense H" begin
-    #     rng = MersenneTwister(123456)
-    #     P = 3
-    #     N = 15
+        # Specify inputs and generate observations.
+        x = collect(range(-3.37, 3.12; length=N))
+        tr_idx = randperm(rng, length(x))[1:2]
+        te_idx = setdiff(eachindex(x), tr_idx)
+        x_tr_raw = x[tr_idx]
+        x_te_raw = x[te_idx]
 
-    #     # Construct a random orthogonal H.
-    #     U, S_diag, _ = svd(randn(rng, P, P))
-    #     H = U * Diagonal(sqrt.(S_diag))
+        # Noise variance.
+        σ² = 0.12
 
-    #     # Specify a collection of GPs
-    #     zs = [GP(Matern52Kernel()) for p in 1:P]
-    #     fs = [sum(H[p, :] .* zs) for p in 1:P]
+        # Construct a random orthogonal H.
+        U, S_diag, _ = svd(randn(rng, P, P))
+        H = U * Diagonal(sqrt.(S_diag))
 
-    #     # Specify equivalent OILMM.
-    #     σ² = 0.11
-    #     D = Diagonal(zeros(P))
-    #     oilmm = OILMM(zs, U, Diagonal(S_diag), σ², D)
+        # Specify a collection of GPs
+        gpc = Stheno.GPC()
+        zs = [wrap(GP(Matern52Kernel()), gpc) for p in 1:P]
+        fs = [sum(H[p, :] .* zs) for p in 1:P]
+        f_naive = Stheno.GPPP(fs, gpc)
 
-    #     # Specify inputs and generate observations.
-    #     x = collect(range(-5.43, 1.76; length=N))
+        x_naive_tr = BlockData(map(p -> GPPPInput(p, x_tr_raw), 1:P))
+        x_naive_te = BlockData(map(p -> GPPPInput(p, x_te_raw), 1:P))
 
-    #     fxs = [f(x, σ²) for f in fs]
-    #     ys = rand(rng, fxs)
-    #     Y = ColVecs(collect(hcat(ys...)'))
+        # Specify equivalent OILMM.
+        D = Diagonal(zeros(P))
+        f = OILMM(zs, U, Diagonal(S_diag), D)
 
-    #     consistency_tests(rng, oilmm, fs, x, Y)
-    # end
+        # Specify inputs and generate observations.
+        x_tr = MOInput(x_tr_raw, P)
+        x_te = MOInput(x_te_raw, P)
+        y_tr = rand(rng, f(x_tr, σ²))
+        y_te = rand(rng, f(x_te, σ²))
+
+        consistency_tests(
+            rng, f, f_naive;
+            x_tr=x_tr,
+            x_te=x_te,
+            x_naive_tr=x_naive_tr,
+            x_naive_te=x_naive_te,
+            y_tr=y_tr,
+            y_te=y_te,
+            σ²=σ²,
+        )
+    end
 end
