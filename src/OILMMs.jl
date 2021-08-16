ebed0696e2b0c6ed2c9d5826641b0081451cd1e5
willtebbutt <wct23@cam.ac.uk>
2021-08-16 21:45:30 +0100

    Update (#11)
    * Bump compat
    
    * Tweak CI conditions
    
    * Add cancel GHA
    
    * Remove redundant deps
    
    * Bump patch

--- src/OILMMs.jl
+++ src/OILMMs.jl
@@ -2,17 +2,13 @@ module OILMMs
 
 using AbstractGPs
 using ChainRulesCore
-using Distributions
 using FillArrays
 using KernelFunctions
 using LinearAlgebra
 using Random
-using Zygote
-using ZygoteRules
 
 using AbstractGPs: AbstractGP, FiniteGP
 using KernelFunctions: MOInput
-
 
 include("util.jl")
 include("oilmm.jl")
--- src/util.jl
+++ src/util.jl
@@ -1,31 +1,26 @@
-ZygoteRules.@adjoint function eachrow(X::VecOrMat)
-    function eachrow_pullback(ΔΩ::NamedTuple)
-        return (ΔΩ.f.A, )
-    end
+function ChainRulesCore.rrule(::typeof(eachrow), X::VecOrMat)
+    eachrow_pullback(ΔΩ::Tangent) = (NoTangent(), ΔΩ.f.A)
     return eachrow(X), eachrow_pullback
 end
 
-ZygoteRules.@adjoint function inv(D::Diagonal{<:Real})
+function ChainRulesCore.rrule(::typeof(inv), D::Diagonal{<:Real})
     Ω = inv(D)
     function inv_Diagonal_pullback(ΔΩ::NamedTuple{(:diag,)})
-        return ((diag = .-ΔΩ.diag .* Ω.diag .^2,), )
+        return (NoTangent(), (diag = .-ΔΩ.diag .* Ω.diag .^2,))
     end
     function inv_Diagonal_pullback(ΔΩ::Diagonal)
-        return (Diagonal(.-ΔΩ.diag .* Ω.diag .^2), )
+        return (NoTangent(), Diagonal(.-ΔΩ.diag .* Ω.diag .^2))
     end
     return Ω, inv_Diagonal_pullback
 end
 
-ZygoteRules.@adjoint function inv(C::Cholesky{<:BLAS.BlasFloat, <:StridedMatrix})
+function ChainRulesCore.rrule(::typeof(inv), C::Cholesky{<:BLAS.BlasFloat, <:StridedMatrix})
     Ω = inv(C)
     function inv_Cholesky_pullback(ΔΩ::StridedMatrix{<:BLAS.BlasFloat})
-        return ((factors = -C.U' \ (ΔΩ + ΔΩ') * Ω, ), )
+        return (NoTangent(), (factors = -C.U' \ (ΔΩ + ΔΩ') * Ω, ))
     end
     function inv_Cholesky_pullback(ΔΩ::AbstractMatrix{<:BLAS.BlasFloat})
         return inv_Cholesky_pullback(collect(ΔΩ))
     end
     return Ω, inv_Cholesky_pullback
 end
-
-Zygote.accum(A::Diagonal, B::NamedTuple{(:diag,)}) = Diagonal(A.diag + B.diag)
-Zygote.accum(A::NamedTuple{(:diag,)}, B::Diagonal) = Zygote.accum(B, A)
